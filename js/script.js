e51db8a4b680c86824b571aeec0513061390d3dd
Gise Haag <ing.giselahaag@gmail.com>
2021-02-25 11:32:53 +0100

    Add default request by location

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 class weatherApp {
-
 	constructor() {
 		this.container = document.querySelector('.container');
-		this.weatherData = {};
 		this.assingElements();
+		this.getGeolocation();
 		this.addEvents();
 	}
 
 	assingElements() {
+		this.apiKey = "c00b4b29cd8af0576010b2bbfd606608";
+
 		this.input = this.container.querySelector('#city');
 		this.button = this.container.querySelector('#button');
+
 		this.location = this.container.querySelector('.location');
 		this.date = this.container.querySelector('.date');
 		this.extremes = this.container.querySelector('.extremes');
@@ -21,32 +23,54 @@ class weatherApp {
 
 	addEvents() {
 		this.button.addEventListener('click', this.getWeather.bind(this));
+
+	}
+
+	async getGeolocation() {
+		if (navigator.geolocation) {
+			try {
+				const position = await this.geolocateUser();
+				const lat = position.coords.latitude;
+				const lon = position.coords.longitude;
+				this.url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
+				this.getWeather();
+
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	}
+
+	geolocateUser() {
+		return new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
 	}
 
 	getWeather() {
 		console.log('consultando...');
 
-		let city = this.input.value;
-		const apiKey = "c00b4b29cd8af0576010b2bbfd606608";
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+		if (this.input.value != '') {
+			const city = this.input.value;
+			this.url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
+		}
 
-		fetch(url)
+		fetch(this.url)
 			.then(response => response.json())
-			.then(data => this.getData(data))
-			.then(this.input.value = '')
-			.then(this.displayData.bind(this));
+			.then(data => {
+				console.log(data);
+				this.weatherData = data;
+				this.input.value = '';
+				this.displayData();
+			})
+			.catch(e => console.error(e));
 	};
 
-	getData(data) {
-		this.weatherData = data;
-	}
-
 	displayData() {
-		console.log(this.weatherData);
-		let tempMinC = Math.round((this.weatherData.main.temp_min - 273.15));
-		let tempMaxC = Math.round((this.weatherData.main.temp_max - 273.15));
-		this.extremes.innerHTML = `Day ${tempMaxC} <span class="icon-long-arrow-up"></span>
-		 									· Night ${tempMinC} <span class="icon-long-arrow-down"></span>`
+		let tempMinC = Math.floor((this.weatherData.main.temp_min - 273.15));
+		let tempMaxC = Math.ceil((this.weatherData.main.temp_max - 273.15));
+		this.extremes.innerHTML = `
+			Day ${tempMaxC} <span class="icon-long-arrow-up"></span> ·
+			Night ${tempMinC} <span class="icon-long-arrow-down"></span>
+		`
 
 		// this.info.innerHTML += `<p>la cuidad consultada es </p>
 		// <img scr="http://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png">`;
