79fd4dbc46ffad1f64eddfbbb9551556f3137251
Gise Haag <ing.giselahaag@gmail.com>
2021-03-04 11:51:39 +0100

    Fix review comments (all at once ¯\_(ツ)_/¯)

--- js/WeatherApp.js
+++ js/WeatherApp.js
@@ -13,14 +13,16 @@ class WeatherApp {
 
 	assingElements() {
 		this.input = this.container.querySelector('#city');
-		this.button = this.container.querySelector('#button');
+		this.form = this.container.querySelector('form');
 		this.mainBox = this.container.querySelector('main');
 		this.errorMessage = this.container.querySelector('.error-message');
 		this.infoApp = this.container.querySelector('.info-app');
+
+		this.input.focus();
 	}
 
 	addEvents() {
-		this.button.addEventListener('click', this.getWeather.bind(this));
+		this.form.addEventListener('submit', this.getWeather.bind(this));
 	}
 
 	async getGeolocation() {
@@ -33,7 +35,7 @@ class WeatherApp {
 				this.getWeather();
 
 			} catch (e) {
-				console.error(e);
+				console.error(e.message, e);
 			}
 		}
 	}
@@ -42,46 +44,73 @@ class WeatherApp {
 		return new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
 	}
 
-	getWeather() {
-		// API Documentation --> https://openweathermap.org/weather-data
-		if (this.input.value != '') {
+	getWeather(e) {
+		e?.preventDefault();
+
+		if (this.input.value.trim() == '') {
+			this.displayErrorMessage('Try writting something 😅');
+		} else {
 			const city = this.input.value;
 			this.url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
+			this.consultAPI();
 		}
+	};
 
+	consultAPI() {
+		// API Documentation --> https://openweathermap.org/weather-data
 		fetch(this.url)
-			.then(response => response.json())
-			.then(data => {
-				if (data.cod == '404') {
-					this.displayErrorMessage(data.message);
-					return;
+			.then((response) => {
+				if (response.status != 200) {
+					throw response.statusText;
 				}
-				// console.log(data);
+				return response.json();
+			})
+			.then(data => {
 				this.weatherData = data;
-				this.input.value = '';
+				this.resetInput();
 				this.displayData();
 			})
 			.catch(e => {
 				console.error(e);
+				this.displayErrorMessage(`Sorry, ${e.toLowerCase()}, please try again 💚`);
 			});
-	};
+	}
 
 	displayErrorMessage(message) {
-		this.errorMessage.innerHTML = `Sorry, ${message}, please try again 💚`;
-		this.infoApp.innerHTML = ``;
-		this.assingElements();
+		this.resetAll();
+		this.errorMessage.innerHTML = message;
+	}
+
+	resetAll() {
+		this.resetBackground();
+		this.resetInput();
+		this.resetData();
+	}
+
+	resetData() {
+		this.infoApp.innerHTML = '';
+	}
+
+	resetInput() {
+		this.input.value = '';
+		this.input.focus();
+	}
+
+	resetErrorField() {
+		this.mainBox.style.height = 'auto';
+		this.errorMessage.innerHTML = '';
 	}
 
 	displayData() {
-		this.mainBox.style.height = 'auto';
-		this.errorMessage.innerHTML = ``;
+		this.resetErrorField();
+		this.parseAPIData();
+		this.createTemplate();
 
-		this.getGeneralInfo();
-		this.getTempertures();
-		this.getWind();
-		this.getIconSet();
+		this.input.setAttribute("placeholder", "try another city");
+	};
+
+	createTemplate() {
 		this.getBackground();
-
 		this.infoApp.innerHTML = `
 			<div>
 				<span class="icon-location1"></span><span>You are in: ${this.city}, ${this.country}</span>
@@ -91,7 +120,7 @@ class WeatherApp {
 
 				<div class="info-wrapper">
 					<p class="small ${this.iconBG}">
-						<img width="35" src="./svg/thermometer.svg" alt="celsius degree" />
+						<img width="35" src="./svg/${this.iconSet.thermometer}" alt="celsius degree" />
 						<span>Day ${this.tempMaxC}º <span class="icon icon-arrow-up"></span></span>
 						<span>Night ${this.tempMinC}º <span class="icon icon-arrow-down"></span></span>
 					</p>
@@ -110,65 +139,20 @@ class WeatherApp {
 
 				<div class="wrapper-info">
 					<p class="small ${this.iconBG}">
-						<img width="35" src="${this.windIcon}" alt="wind-icon"/>
+						<img width="35" src="./svg/${this.windIcon}" alt="wind-icon"/>
 						<span>Wind: ${this.windKMH} km/h from ${this.windDir}</span>
 					</p>
 					<p class="small ${this.iconBG}">
-						<img width="35" src="./svg/humidity.svg" alt="humidity-icon"/>
+						<img width="35" src="./svg/${this.iconSet.humidity}" alt="humidity-icon"/>
 						<span>Humidity: ${this.humidity} % </span>
 					</p>
 					<p class="small ${this.iconBG}">
-						<img width="35" src="./svg/barometer.svg" alt="barometer-icon"/>
+						<img width="35" src="./svg/${this.iconSet.barometer}" alt="barometer-icon"/>
 						<span>Pressure: ${this.pressure} hPa</span>
 					</p>
 				</div>
 			</div>
 		`;
-
-		this.input.setAttribute("placeholder", " try another city");
-	};
-
-	getGeneralInfo() {
-		const SEC_IN_HOUR = 3600;
-		const timezone = this.weatherData.timezone / SEC_IN_HOUR;
-
-		this.city = this.weatherData.name;
-		this.country = this.weatherData.sys.country;
-		this.description = this.weatherData.weather[0].description;
-		this.pressure = this.weatherData.main.pressure;
-		this.humidity = this.weatherData.main.humidity;
-
-		this.currentDate = moment().utcOffset(timezone).format('MMMM DD, HH:mm');
-	}
-
-	getTempertures() {
-		const KELVIN = 273.15;
-		this.tempMinC = Math.floor((this.weatherData.main.temp_min - KELVIN));
-		this.tempMaxC = Math.ceil((this.weatherData.main.temp_max - KELVIN));
-		this.temp = Math.floor((this.weatherData.main.temp - KELVIN));
-		this.feels = Math.floor((this.weatherData.main.feels_like - KELVIN));
-	}
-
-	getWind() {
-		const MStoKMH = 3.6;
-		const MAX_DIFF = 360;
-		const windSpeed = this.weatherData.wind.speed;
-		this.windIcon = (windSpeed < 15) ? './svg/wind.svg' : './svg/wind-sing.svg';
-		this.windKMH = Math.round(windSpeed * MStoKMH);
-
-		let diff = MAX_DIFF;
-		let closer;
-
-		Object.keys(windDirection).forEach(key => {
-			let currentDiff = Math.abs(key - this.weatherData.wind.deg);
-
-			if (currentDiff < diff) {
-				diff = currentDiff;
-				closer = key;
-			}
-		});
-
-		this.windDir = windDirection[closer];
 	}
 
 	getIconSet() {
@@ -192,6 +176,66 @@ class WeatherApp {
 		const backgroundColor = background[dayOrNight][clearOrCloudy];
 		this.container.style.background = backgroundColor;
 	};
+
+	resetBackground() {
+		this.container.style.color = '#000';
+		this.container.style.background = 'linear-gradient(#bbb, #fff)';
+	}
+
+	parseAPIData() {
+		this.getGeneralInfo();
+		this.getTempertures();
+		this.getWind();
+	}
+
+	getGeneralInfo() {
+		const SEC_IN_HOUR = 3600;
+		const timezone = this.weatherData.timezone / SEC_IN_HOUR;
+
+		this.city = this.weatherData.name;
+		this.country = this.weatherData.sys.country;
+		this.description = this.weatherData.weather[0].description;
+		this.pressure = this.weatherData.main.pressure;
+		this.humidity = this.weatherData.main.humidity;
+
+		this.currentDate = moment().utcOffset(timezone).format('MMMM DD, HH:mm');
+	}
+
+	convertKtoC(temperature, type = "floor") {
+		const KELVIN = 273.15;
+		return Math[type]((temperature - KELVIN));
+	}
+
+	getTempertures() {
+		this.tempMinC = this.convertKtoC(this.weatherData.main.temp_min);
+		this.tempMaxC = this.convertKtoC(this.weatherData.main.temp_max, 'ceil');
+		this.temp = this.convertKtoC(this.weatherData.main.temp);
+		this.feels = this.convertKtoC(this.weatherData.main.feels_like);
+	}
+
+	getWind() {
+		this.getIconSet();
+
+		const MS_TO_KMH = 3.6;
+		const MAX_DIFF = 360;
+		const windSpeed = this.weatherData.wind.speed;
+		this.windIcon = (windSpeed < 15) ? this.iconSet.wind : this.iconSet.windsign;
+		this.windKMH = Math.round(windSpeed * MS_TO_KMH);
+
+		let diff = MAX_DIFF;
+		let closer;
+
+		Object.keys(windDirection).forEach(key => {
+			let currentDiff = Math.abs(key - this.weatherData.wind.deg);
+
+			if (currentDiff < diff) {
+				diff = currentDiff;
+				closer = key;
+			}
+		});
+
+		this.windDir = windDirection[closer];
+	}
 }
 
 const app = new WeatherApp();
